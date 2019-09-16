9efdd0a09efaf9c021b84bd884048dcfc4e7cb7a
BikerGitHub <22326595+BikerGitHub@users.noreply.github.com>
2019-09-16 14:38:10 +0800

    class result

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,26 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+from django.utils import timezone
 
+class Post(models.Model):
+    title=models.CharField(max_length=200)
+    slug=models.CharField(max_length=200)
+    body=models.TextField()
+    pub_date=models.DateTimeField(default=timezone.now)
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-# from django.utils import timezone
-
-# class Post(models.Model):
-#     title=models.CharField(max_length=200)
-#     slug=models.CharField(max_length=200)
-#     body=models.TextField()
-#     pub_date=models.DateTimeField(default=timezone.now)
-
-#     class Meta:
-#         ordering=('-pub_date',)
+    class Meta:
+        ordering=('-pub_date',)
     
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.title
