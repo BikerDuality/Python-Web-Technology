c72d72a7c48c6c2cb0350aa76ff1b0b62e662aa1
BikerGitHub <22326595+BikerGitHub@users.noreply.github.com>
2019-09-25 10:47:41 +0800

    using template show post

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404
+import datetime
 from .models import Post
 
 def home_page(request):
     posts=Post.objects.all()
-    post_list=[]
-    for count,post in enumerate(posts):
-        post_list.append('No.'+str(count+1)+'\t'+post.title+'<br>')
-        post_list.append(post.slug+'<hr>')
-        post_list.append('<pre>'+str(count+1)+post.body+'</pre><br><br>')
-    return HttpResponse(post_list)
+    template=loader.get_template('index.html')
+    now=datetime.datetime.now()
+    html=template.render(locals()).encode()
+    return HttpResponse(html)
+
+def show_post(request,post_name):
+    template=loader.get_template('post.html')
+    post=get_object_or_404(Post,slug=post_name)
+    html=template.render(locals()).encode()
+    return HttpResponse(html)
